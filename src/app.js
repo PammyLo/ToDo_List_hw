8d6ef887c537f83f1f3fda5db9266e9b49dffeec
PammyLo <pamela@tuminds.com>
2019-10-14 19:50:07 +0100

    ability to add todo item added- MVP done

--- src/app.js
+++ src/app.js
@@ -4,11 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-      actions:[ "Something", "More stuff" ],
-      newAction: ""
+      todos:[ "Buy shopping", "Clean bathroom", "Car's MOT" ],
+      newTodo: ""
 },
-    // methods: {
-    //
-    // }
+    methods: {
+      saveNewTodo: function (newTodo) {
+        this.todos.push(this.newTodo);
+        this.newTodo = "";
+      }
+    }
   });
 });
