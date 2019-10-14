97c1725eb085e9a75a1fd6017eec5b1a422b8a08
PammyLo <pamela@tuminds.com>
2019-10-14 16:52:46 +0100

    added list of actions

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: "#app",
+    data: {
+      actions:[ "Something", "More stuff" ],
+      newAction: ""
+},
+    // methods: {
+    //
+    // }
+  });
+});
